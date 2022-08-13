65475e1caf13a24a0da2a2d90c87ef904e032b7b
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-13 18:52:53 +0300

    завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const refs = {
+    startButton: document.querySelector("button[data-start]"),
+    stopButton: document.querySelector("button[data-stop]"),
+}
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+    refs.startButton.setAttribute('disabled', true);
+    refs.stopButton.removeAttribute('disabled');
+    intervalId = setInterval(changeBgColor, 1000);
+}
+
+function onStopButtonClick() {
+    clearInterval(intervalId);
+    refs.stopButton.setAttribute('disabled', true);
+    refs.startButton.removeAttribute('disabled');
+}
